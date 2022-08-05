b881df6cc3214df17c56c868d6c27bff5d02ebcf
sslime336 <97684920+sslime336@users.noreply.github.com>
2022-08-05 11:16:09 +0800

    logger/logger.go: finish

--- logger/logger.go
+++ logger/logger.go
@@ -1,6 +1,9 @@
 package logger
 
-// WeakLogger only has methods Info, Warn and Error.
+import "log"
+
+// WeakLogger only has methods Info, Warn and Error
+// as it is only used inner the module iter
 type WeakLogger interface {
 	Info(msg ...any)
 	Warn(msg ...any)
@@ -12,12 +15,15 @@ var _defaultLogger WeakLogger = &weakLogger{}
 type weakLogger struct{}
 
 func (wl *weakLogger) Info(msg ...any) {
+	log.Println(msg...)
 }
 
 func (wl *weakLogger) Warn(msg ...any) {
+	log.Println(msg...)
 }
 
 func (wl *weakLogger) Error(msg ...any) {
+	log.Println(msg...)
 }
 
 func SetLogger(l WeakLogger) {
