50c2ed0504f285f6abe576f6643fc3a3b68eeed2
sslime336 <97684920+sslime336@users.noreply.github.com>
2022-08-05 11:34:12 +0800

    Add test/iter_test.go

---
+++ test/iter_test.go
@@ -0,0 +1,22 @@
+package iter
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/sslime336/iter"
+	"github.com/sslime336/iter/slice"
+)
+
+type ints struct {
+	inner []int
+}
+
+func (i *ints) Iter() iter.SliceIter[int] {
+	return slice.Iter(i.inner)
+}
+
+func TestIter(t *testing.T) {
+	intvar := ints{inner: []int{1, 2, 3, 4, 5}}
+	fmt.Println(intvar.Iter().Count())
+}
